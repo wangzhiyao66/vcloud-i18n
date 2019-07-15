c7ca86335180e630733ce8221c43079007df28b1
wangzhiyao66 <wangzhiyao66@sina.com>
2019-07-15 20:53:49 +0800

    添加基本插件库 和基本插件库配置

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 // AoT requires an exported function for factories
@@ -13,13 +15,24 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+// tslint:disable-next-line:jsdoc-format
+/** 配置 angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    // tslint:disable-next-line:jsdoc-format
+    /** 导入 ng-zorro-antd 模块 **/
+    NgZorroAntdModule,
     // ---------------添加语言代码
     TranslateModule.forRoot({
       loader: {
@@ -29,7 +42,11 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
       }
     })
   ],
-  providers: [],
+   // tslint:disable-next-line:jsdoc-format
+   /** 配置 ng-zorro-antd 国际化（文案 及 日期） **/
+   providers   : [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
