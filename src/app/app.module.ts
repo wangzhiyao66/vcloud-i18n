6c1b950b8fb0b52a308b12d53a4f4c6ef829d992
wangzhiyao66 <wangzhiyao66@sina.com>
2019-07-15 20:25:14 +0800

    添加语言文件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// 自定义依赖所需导入
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+       // ---------------添加以下代码
+       TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+      // --------------------------
   ],
   providers: [],
   bootstrap: [AppComponent]
