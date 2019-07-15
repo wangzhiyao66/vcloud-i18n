9a6049a5ca4c645b189b2e9947e942db93ac8133
wangzhiyao66 <wangzhiyao66@sina.com>
2019-07-15 20:38:34 +0800

    添加语言转换

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vcloud-i18n';
+
+  constructor(
+    public translate: TranslateService
+  ) { }
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  public async ngOnInit() {
+    // 语言初始化(若未设置语言, 则取浏览器语言)
+    const currentLanguage = await localStorage.getItem('currentLanguage') || this.translate.getBrowserCultureLang();
+    // 当在assets/i18n中找不到对应的语言翻译时，使用'zh-CN'作为默认语言
+    this.translate.setDefaultLang('zh-CN');
+    this.translate.use(currentLanguage);
+    // 记录当前设置的语言
+    localStorage.setItem('currentLanguage', currentLanguage);
+  }
+
+  // 设置语言
+  public selectLanguage(lang) {
+    this.translate.use(lang);
+    // 更新当前记录的语言
+    localStorage.setItem('currentLanguage', lang);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 // 自定义依赖所需导入
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
@@ -14,15 +19,15 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
   ],
   imports: [
     BrowserModule,
-       // ---------------添加以下代码
-       TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
-      })
-      // --------------------------
+    HttpClientModule,
+    // ---------------添加语言代码
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
